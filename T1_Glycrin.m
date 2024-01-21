a4e7cd47aa76970f08602c6bef651a9265e522bd
HKK1809 <153373265+HKK1809@users.noreply.github.com>
2024-01-21 11:15:38 +0530

    Add files via upload

---
+++ T1_Glycrin.m
@@ -0,0 +1,40 @@
+% This file is for analysing T1 for Mineral Oil 
+
+% Time is in ms, Voltage is in volts
+
+time = GlycerinT1(:,1);
+time = table2array(time);
+err_time = (2E-08)*ones(size(time));
+
+voltage =  GlycerinT1(:,2);
+voltage = table2array(voltage);
+err = 0.08*ones(size(voltage));
+
+%F = fit(time,voltage,'(a*(1-exp(-x/b)))+c','Start', [100, 2, 0],'Weight',1/0.02^2+zeros(length(time),1));
+[F,goF,Fit_output] = fit(time,voltage,'(a*(1-exp(-x/b)))+c','Start', [100, 2, 0],'Weight',err.^(-2));
+
+plot(F,time,voltage)
+xlabel('Time (ms)','FontSize',18);
+ylabel('Potential Observed (V)','FontSize',18);
+title ('Variation in Pulse Amplitude with increasing time Delay between Pulses','FontSize',18)
+ 
+legend('Spin-Lattice Time Delay for Glycerin', 'T1 = (40.8 +/- 1.1)ms','FontSize',18)
+hold on
+errorbar(time, voltage, err,"k.",'HandleVisibility','off')
+hold on
+errorbar(time, voltage, err_time,'horizontal', "k.",'HandleVisibility','off')
+hold 
+
+% Extract weighted jacobian
+J = Fit_output.Jacobian;
+
+%Get the covariance and curvature matrix and extract the errors on F
+%parameters from there.
+curvature_matrix = J'*J;
+covariance_matrix = inv(curvature_matrix);
+
+% Calculate CHI_squared
+min_chi2 = goF.sse;
+dof = goF.dfe;
+
+reduced_chi2 = min_chi2/dof;
---
+++ T2_Moil.m
@@ -0,0 +1,46 @@
+% This file is for analysing T2 for Mineral Oil
+% Time in ms Volatge in Volts
+
+% This is measured data analysis
+time_measured = MoilT2(:,1);
+time_measured = table2array(time_measured);
+
+voltage_measured =  MoilT2(:,2);
+voltage_measured = table2array(voltage_measured);
+
+[F,goF,Fit_output] = fit(time_measured,voltage_measured,'(a*(exp(-x/b)))+c');
+
+err = 0.08*ones(size(voltage_measured));
+errorbar(time_measured, voltage_measured, err,'vertical',"k.",'HandleVisibility','off')
+hold on
+plot(F,time_measured,voltage_measured)
+xlabel('Time (ms)','FontSize',18);
+ylabel('Potential Observed (V)','FontSize',18);
+title ('Variation in Pulse Amplitude in accordance with the Nth B Pulse ','FontSize',18)
+
+legend('Spin-Spin Time Delay for Mineral Oil', 'T2 = (10 +/- 26)ms','FontSize',18)
+
+% Extract weighted jacobian
+J = Fit_output.Jacobian;
+
+%Get the covariance and curvature matrix and extract the errors on F
+%parameters from there.
+curvature_matrix = J'*J;
+covariance_matrix = inv(curvature_matrix);
+
+% Calculate CHI_squared
+min_chi2 = goF.sse;
+dof = goF.dfe;
+
+reduced_chi2 = min_chi2/dof;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+MoilextractT2 = 'MoilT2extracted1527825.isf';
+MoilextractT2 = isfread(MoilextractT2);
+time_extracted = MoilextractT2.x;
+voltage_extracted = MoilextractT2.y;
+
+figure(2)
+plot(time_extracted, voltage_extracted)
+xlabel('Time (s)','FontSize',18);
+ylabel('Potential Observed (V)','FontSize',18);
+title ('Pulse Echo signal Obtained from interaction of 15 Pulses ','FontSize',18)
